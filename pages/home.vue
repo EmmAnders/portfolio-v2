639ed33b4cbd1053b3c9e4bcae2953cb809018a5
Emma <emmandersson166@gmail.com>
2021-11-13 14:18:18 +0100

    home page created

---
+++ pages/home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home-container row">
+    <p class="double-column actiom">
+      Front-End Developer <br />
+      Based in Copenhagen.
+    </p>
+    <p class="column"></p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({}),
+};
+</script>
+
+<style lang="scss">
+.home-container {
+  height: 100%;
+  display: flex;
+  align-items: flex-end;
+  @include copy-l;
+  text-indent: 3rem;
+}
+</style>
