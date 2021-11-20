5e28a0c2134d7ec654e6e61329086143530cff23
Emma <emmandersson166@gmail.com>
2021-11-20 16:48:48 +0100

    -

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,10 @@
 export default {
   target: "static",
   ssr: false,
+
+  generate: {
+    fallback: true,
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: "EMMA",
